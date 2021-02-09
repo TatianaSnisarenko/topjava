62fb7e0ba375a0dd19792a37b6d36f6282ba44dc
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2021-02-09 18:52:38 +0200

    apply patch 1_2_switch_to_war.patch

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -26,6 +26,7 @@ public class MealsUtil {
 
         List<MealTo> mealsTo = filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo.forEach(System.out::println);
+        
     }
 
     public static List<MealTo> filteredByStreams(List<Meal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
