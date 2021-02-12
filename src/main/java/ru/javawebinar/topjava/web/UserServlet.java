c86d1aceea1c4c64884c3a0b736946e0c194f72b
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2021-02-12 11:54:38 +0200

    add Completed HW1

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -15,9 +15,10 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("redirect to users");
+        log.debug("forward to users");
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
 
 //        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("users.jsp");
+ //       response.sendRedirect("users.jsp");
     }
 }
