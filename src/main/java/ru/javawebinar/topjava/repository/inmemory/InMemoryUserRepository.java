f1f326404bdb848a6887b07daa70c0774a6a2c71
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2021-02-19 16:45:38 +0200

    apply 3_0_fix.patch

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -8,9 +8,15 @@ import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
+    private final Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(0);
+    
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 
     @Override
